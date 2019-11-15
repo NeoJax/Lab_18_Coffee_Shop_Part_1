975086a534a9987dc1371bda4c02ac95484ad22f
Johnathon Lubaway <NeoJax@gmail.com>
2019-11-15 13:12:18 -0500

    Finished Part 1

---
+++ Lab_18_Coffee_Shop_Part_1/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_18_Coffee_Shop_Part_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lab_18_Coffee_Shop_Part_1.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Summary(RegisterUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(user);
+            }
+            return View("Registration");
+        }
+    }
+}
---
+++ Lab_18_Coffee_Shop_Part_1/Models/RegisterUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_18_Coffee_Shop_Part_1.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 5)]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"(?=.*[!@#$%^&*])(?=.*\d)(?=.*[A-Z]).{8,}", ErrorMessage = "The password must contain a symbol(!@#$%^&*), a number, a capital letter, and must be atleast 8 characters." )]
+        public string Password { get; set; }
+    }
+}
